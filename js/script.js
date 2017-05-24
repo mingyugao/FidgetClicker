51579585f725d5729a2d683e1338b8502ed0c31b
mingyugao <mingyu.gao96@gmail.com>
2017-05-24 15:40:08 -0700

    adds stats box and colors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var spins = 0;
+var total_spins = 0;
+var spent = 0;
 function spin() {
   if (spins == 0) {
     first_click = Date();
   }
   spins += 1;
+  total_spins += 1;
   document.getElementById("spins").innerText = spins + " spins";
 }
 document.getElementById("spins").innerText = spins + " spins";
-
